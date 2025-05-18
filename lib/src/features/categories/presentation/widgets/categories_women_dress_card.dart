e4c81c4c87223d4d9b1dcece23ab5a60a91174b8
imsisojib <sirajulislam1123@gmail.com>
2025-05-18 18:29:49 +0600

    Added bottom favourite icon in the product card

--- lib/src/features/categories/presentation/widgets/categories_women_dress_card.dart
+++ lib/src/features/categories/presentation/widgets/categories_women_dress_card.dart
@@ -53,6 +53,27 @@ final List<ProductModel> productList = [
     price: '25',
     image: AppImages.blackDress,
   ),
+  ProductModel(
+    title: 'T-shirts',
+    company: 'Mango',
+    rating: '5',
+    price: '23',
+    image: AppImages.streetCloths,
+  ),
+  ProductModel(
+    title: 'Crop tops',
+    company: 'Dorothy Perkins',
+    rating: '4',
+    price: '20',
+    image: AppImages.blackDress2,
+  ),
+  ProductModel(
+    title: 'Sleeveless',
+    company: 'LOST Ink',
+    rating: '5',
+    price: '43',
+    image: AppImages.banner,
+  ),
 ];
 
 class DressCard extends StatelessWidget {
@@ -65,57 +86,90 @@ class DressCard extends StatelessWidget {
     final theme = Theme.of(context);
     final int ratingValue = int.tryParse(product.rating) ?? 0;
 
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
-        boxShadow: [
-          BoxShadow(color: Colors.black12, blurRadius: 4, offset: Offset(0, 2)),
-        ],
-      ),
-      child: Row(
-        children: [
-          SizedBox(
-            width: 150,
-            height: 100,
-            child: ClipRRect(
-              child: Image.asset(product.image, fit: BoxFit.cover),
-            ),
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black12, blurRadius: 4, offset: Offset(0, 2)),
+            ],
           ),
-          SizedBox(width: 12),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(product.title, style: theme.textTheme.bodyLarge),
-                SizedBox(height: 6),
-                Text(product.company,
-                    style: theme.textTheme.labelSmall
-                        ?.copyWith(color: Colors.grey)),
-                Row(
+          child: Row(
+            children: [
+              SizedBox(
+                width: 150,
+                height: 100,
+                child: ClipRRect(
+                  child: Image.asset(product.image, fit: BoxFit.cover),
+                ),
+              ),
+              SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    ...List.generate(5, (index) {
-                      return Icon(
-                        index < ratingValue ? Icons.star : Icons.star_border,
-                        size: 13,
-                        color: index < ratingValue ? Colors.amber : Colors.grey,
-                      );
-                    }),
-                    SizedBox(width: 4),
-                    Text(
-                      '(${product.rating})',
-                      style: theme.textTheme.labelSmall
-                          ?.copyWith(color: Colors.grey),
+                    Text(product.title, style: theme.textTheme.bodyLarge),
+                    SizedBox(height: 6),
+                    Text(product.company,
+                        style: theme.textTheme.labelSmall
+                            ?.copyWith(color: Colors.grey)),
+                    Row(
+                      children: [
+                        ...List.generate(5, (index) {
+                          return Icon(
+                            index < ratingValue
+                                ? Icons.star
+                                : Icons.star_border,
+                            size: 13,
+                            color: index < ratingValue
+                                ? Colors.amber
+                                : Colors.grey,
+                          );
+                        }),
+                        SizedBox(width: 4),
+                        Text(
+                          '(${product.rating})',
+                          style: theme.textTheme.labelSmall
+                              ?.copyWith(color: Colors.grey),
+                        ),
+                      ],
                     ),
+                    Text('${product.price}\$',
+                        style: theme.textTheme.labelLarge),
                   ],
                 ),
-                Text('${product.price}\$', style: theme.textTheme.labelLarge),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: -0,
+          right: 25,
+          child: Container(
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black12, blurRadius: 4, offset: Offset(0, 2)),
               ],
             ),
+            child: const Padding(
+              padding: EdgeInsets.all(8),
+              child: Icon(
+                Icons.favorite_border,
+                color: Colors.grey,
+                size: 15,
+              ),
+            ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
