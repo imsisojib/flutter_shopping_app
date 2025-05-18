8345f7aa0f334c43bc5f504ad18722990615d56f
imsisojib <sirajulislam1123@gmail.com>
2025-05-18 12:39:30 +0600

    Categories List page design completed

--- lib/src/features/categories/presentation/screens/screen_category_list.dart
+++ lib/src/features/categories/presentation/screens/screen_category_list.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/categories_name.dart';
+
 class ScreenCategoryList extends StatelessWidget {
   const ScreenCategoryList({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.arrow_back),
@@ -13,7 +16,57 @@ class ScreenCategoryList extends StatelessWidget {
         centerTitle: true,
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
       ),
-      body: Text('data'),
+      body: CustomScrollView(
+        slivers: [
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "View All Items",
+                    style: theme.textTheme.bodyMedium
+                        ?.copyWith(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 130, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+                child: Text(
+                  'Choose Category',
+                  style:
+                      theme.textTheme.bodyMedium?.copyWith(color: Colors.grey),
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                height: 2,
+                color: Colors.grey[300],
+              ),
+              CategoryName(title: 'Tops'),
+              CategoryName(title: 'Shirts & Blouses'),
+              CategoryName(title: 'Cardigans & Sweaters'),
+              CategoryName(title: 'Knitwear'),
+              CategoryName(title: 'Blazers'),
+              CategoryName(title: 'Outerwear'),
+              CategoryName(title: 'Pants'),
+              CategoryName(title: 'Jeans'),
+              CategoryName(title: 'Shorts'),
+              CategoryName(title: 'Skirts'),
+              CategoryName(title: 'Dresses'),
+            ]),
+          )
+        ],
+      ),
     );
   }
 }
